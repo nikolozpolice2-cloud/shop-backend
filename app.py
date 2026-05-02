a4d73e16bde2e25b65a245621936b5e0bee844a5
nikolozpolice2-cloud <nikoloz.police.2@gmail.com>
2026-05-03 03:26:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "API WORKING"
 
 @app.route("/order", methods=["POST"])
 def order():
     data = request.json
     print("ORDER RECEIVED:", data)
     return jsonify({"status": "ok"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
