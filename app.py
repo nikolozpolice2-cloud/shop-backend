a27529fd080c89a8427993858e28efb76a6156e7
nikolozpolice2-cloud <nikoloz.police.2@gmail.com>
2026-05-03 03:40:43 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+orders = []  # აქ ინახება ყველა შეკვეთა
+
 @app.route("/")
 def home():
     return "API WORKING"
@@ -11,7 +13,14 @@ def home():
 @app.route("/order", methods=["POST"])
 def order():
     data = request.json
-    print("ORDER RECEIVED:", data)
+    orders.append(data)   # ⬅️ ინახავს order-ს
+
+    print("NEW ORDER:", data)
     return jsonify({"status": "ok"})
+
+@app.route("/orders", methods=["GET"])
+def get_orders():
+    return jsonify(orders)  # ⬅️ აქ ნახავ ყველას
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
