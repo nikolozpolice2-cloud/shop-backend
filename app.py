803725d61826110159eaf375c7f4b485c39efda6
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2026-05-03 00:02:14 -0300

    Add business rules

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from guara import application, it
+
+from transactions import CreateOrder, OrderDoesNotExist, TransactionException 
 
 app = Flask(__name__)
 CORS(app)
 
 orders = []  # აქ ინახება ყველა შეკვეთა
+
+brain = application.Application()
 
 @app.route("/")
 def home():
@@ -13,10 +18,18 @@ def home():
 @app.route("/order", methods=["POST"])
 def order():
     data = request.json
-    orders.append(data)   # ⬅️ ინახავს order-ს
+    try:
+        (
+            brain.given(OrderDoesNotExist, orders=orders, order=data)
+            .when(CreateOrder, orders=orders, order=data)
+            .then(it.IsTrue)
+        )
 
-    print("NEW ORDER:", data)
-    return jsonify({"status": "ok"})
+        print("NEW ORDER:", data)
+        return jsonify({"status": "ok"})
+    except TransactionException as e:
+        return jsonify({"status": "fail", "error": str(e)})
+         
 
 @app.route("/orders", methods=["GET"])
 def get_orders():
